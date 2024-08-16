af29384e6be78905e9d7be8d64c6806b6234c3db
hustlerZzZ <rishabpal2000@gmail.com>
2024-08-16 20:03:19 +0530

    Initial Commit

---
+++ app/layout.jsx
@@ -0,0 +1,37 @@
+import Header from "@/app/_components/Header";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+    subsets: ["latin"],
+    display: "swap",
+});
+
+import { ReservationProvider } from "@/app/_hooks/ReservationContext";
+import "@/app/_styles/globals.css";
+
+export const metadata = {
+    title: {
+        template: "%s / The Wild Oasis",
+        default: "Welcome / The Wild Oasis",
+    },
+    description:
+        "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body
+                className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen antialiased flex flex-col`}
+            >
+                <Header />
+
+                <div className="flex-1 px-8 py-12 grid">
+                    <main className="max-w-7xl mx-auto w-full">
+                        <ReservationProvider>{children}</ReservationProvider>
+                    </main>
+                </div>
+            </body>
+        </html>
+    );
+}
